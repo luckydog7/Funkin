fc21b284bf39729e4a36d9cccfe89408ea3ac424
melanie89988 <80945262+melanie89988@users.noreply.github.com>
2021-07-23 17:00:07 -0500

    Update MenuCharacter.hx

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -7,26 +7,31 @@ class MenuCharacter extends FlxSprite
 {
 	public var character:String;
 
+	private var listCharacters = ['dad'];
+
 	public function new(x:Float, character:String = 'bf')
 	{
 		super(x);
 
 		this.character = character;
 
-		var tex = Paths.getSparrowAtlas('campaign_menu_UI_characters');
+		var tex = Paths.getSparrowAtlas('menucharacter/campaign_menu_UI_characters');
+		
+		for (char in listCharacters){
+			var chartex = Paths.getSparrowAtlas('menucharacter/' + char);
+				
+			for (frame in chartex.frames){
+				tex.pushFrame(frame);
+			}
+		}
+
 		frames = tex;
 
 		animation.addByPrefix('bf', "BF idle dance white", 24);
 		animation.addByPrefix('bfConfirm', 'BF HEY!!', 24, false);
 		animation.addByPrefix('gf', "GF Dancing Beat WHITE", 24);
+		animation.addByPrefix('gar', "garcello idle", 24);
 		animation.addByPrefix('dad', "Dad idle dance BLACK LINE", 24);
-		animation.addByPrefix('spooky', "spooky dance idle BLACK LINES", 24);
-		animation.addByPrefix('pico', "Pico Idle Dance", 24);
-		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
-		animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24);
-		animation.addByPrefix('senpai', "SENPAI idle Black Lines", 24);
-		animation.addByPrefix('tankman', "Tankman Menu BLACK", 24);//for template use.
-		// Parent Christmas Idle
 
 		animation.play(character);
 		updateHitbox();
